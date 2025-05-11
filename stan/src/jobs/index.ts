c937cf2fbce77efa97a46d1c7d6ad64287d532ae
Thomas Arsenault <thomasarsenaultj@gmail.com>
2025-05-10 22:51:49 -0400

    ⚡ only run cron jobs on my fake prod env

--- stan/src/jobs/index.ts
+++ stan/src/jobs/index.ts
@@ -2,6 +2,11 @@ import cron from 'node-cron';
 import { migrateTasks, moveBacklogBack } from './tasks';
 
 const scheduler = () => {
+    if (process.env.NODE_ENV === 'development') {
+        console.log('Skipping scheduled jobs in development environment');
+        return;
+    }
+
     cron.schedule('0 4 * * *', () => { // nightly at 4am
         migrateTasks();
         moveBacklogBack();
